00f42fee1e5e70591679d3e7d294d08f67ce92d6
Jekman <jeka_zzz@mail.ru>
2020-06-19 17:09:26 +0300

    main-page/feat: create base elements layout

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
 import './plugins/bootstrap/index';
 import './plugins/fontawesome/index';
-
-import './scss/_constants.scss';
-import './scss/_mixins.scss';
-import './core/Component';
-import './core/DomListener';
-import './core/domManipulation';
-import './core/Observer';
-import './core/utils';
-
-console.log('Help me!');
