749ecd8b694632e07c314ccfc1c9a99cb7100a97
julia-dream <55105383+julia-dream@users.noreply.github.com>
2020-06-21 13:28:55 +0300

    authorization/feat: improve validity checking

--- src/components/Authorization/checkTokenValidity.js
+++ src/components/Authorization/checkTokenValidity.js
@@ -1,9 +1,12 @@
 export default function checkTokenValidity() {
   const currentToken = localStorage.getItem('currentToken');
-  const tokenExpiresIn = localStorage.getItem('tokenExpiresIn');
+  const tokenExpiresIn = Number(localStorage.getItem('tokenExpiresIn'));
   const now = new Date();
 
-  if (!currentToken || !tokenExpiresIn || now.getTime() - tokenExpiresIn.getTime() > 0) {
+  if (!currentToken || !tokenExpiresIn) {
+    return false;
+  }
+  if (now.getTime() - tokenExpiresIn > 0) {
     return false;
   }
 
