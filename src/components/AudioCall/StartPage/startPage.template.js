29658486986d9e2b0f274f14d8ef99a603450a10
julia-dream <55105383+julia-dream@users.noreply.github.com>
2020-06-21 22:09:50 +0300

    audiocall/init: create basic structure

---
+++ src/components/AudioCall/StartPage/startPage.template.js
@@ -0,0 +1,39 @@
+export default function createStartPage() {
+  return `
+    <div class="row pt-3">
+        <div class="col-11"></div>
+        <div class="col">
+            <button class="btn" type="submit">
+            <i class="fas fa-times"></i>
+            </button>
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col"></div>
+        <div class="col">
+            <p class="my-5 text-uppercase text-center text-white display-4 game-title">
+            Аудиовызов
+            </p>
+        </div>
+        <div class="col"></div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <p class="text-center text-white display-5 game-desc">
+            Тренировка улучшает восприятие английской речи на слух.
+            </p>
+        </div>
+    </div>
+    <div class="row my-5">
+        <div class="col"></div>
+        <div class="col">
+            <button type="button" class="d-block btn btn-outline-light ml-auto mr-auto text-uppercase btn-start-game">
+            Начать
+            </button>
+        </div>
+        <div class="col"></div>
+    </div>
+    `;
+}
+
+/* <div class="container-fluid audio-call-start-page"></div> */
