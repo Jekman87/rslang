bbc9323a05fad19735d43e675e5d4b810b49f69b
Jekman <jeka_zzz@mail.ru>
2020-06-23 14:44:58 +0300

    api/feat: implement words methods

---
+++ src/api/Api.js
@@ -0,0 +1,81 @@
+const baseUrl = 'https://afternoon-falls-25894.herokuapp.com';
+
+export default class Api {
+  constructor(token, userId, exp) {
+    this.token = token;
+    this.userId = userId;
+    this.exp = exp;
+  }
+
+  async loginUser(userLog) {
+    const rawResponse = await fetch(`${baseUrl}/signin`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userLog),
+    });
+
+    const content = await rawResponse.json();
+
+    const now = new Date();
+    content.tokenExpiresIn = now.setHours(now.getHours() + 4);
+
+    // запись в LocalStorage токен
+    // и время истечения?
+    this.token = content.token;
+    this.userId = content.userId;
+
+    return content;
+  }
+
+  // Words methods
+  async getWords(page = 0, group = 0, wordsPerExampleSentenceLTE, wordsPerPage = 10) {
+    let url = `${baseUrl}/words?page=${page}&group=${group}`;
+
+    if (wordsPerExampleSentenceLTE !== undefined) {
+      url += `&wordsPerExampleSentenceLTE=${wordsPerExampleSentenceLTE}&wordsPerPage=${wordsPerPage}`;
+    }
+
+    const rawResponse = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    const content = await rawResponse.json();
+
+    return content;
+  }
+
+  async getWordsCount(page = 0, wordsPerExampleSentenceLTE, wordsPerPage = 10) {
+    let url = `${baseUrl}/words/count?page=${page}`;
+
+    if (wordsPerExampleSentenceLTE !== undefined) {
+      url += `&wordsPerExampleSentenceLTE=${wordsPerExampleSentenceLTE}&wordsPerPage=${wordsPerPage}`;
+    }
+
+    const rawResponse = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    const content = await rawResponse.json();
+
+    return content;
+  }
+
+  async getWordById(wordId) {
+    const rawResponse = await fetch(`${baseUrl}/words/${wordId}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    const content = await rawResponse.json();
+
+    return content;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -24,3 +24,19 @@ const pages = { Authorization, MainPage, MainGame };
 
 const mainApp = new MainApp('#app', { components: [Header, PageContainer], pages, startPage });
 mainApp.render();
+
+// test api
+import Api from './api'
+
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlZjE1NTM0YWEyNDVlMDAxN2E1Nzk0MyIsImlhdCI6MTU5Mjg3NDYzOCwiZXhwIjoxNTkyODg5MDM4fQ.E2YtDIU6aU9bz_Kg588ixfcI5DLWo96mNgCJkOomVLs';
+const userId = '5ef15534aa245e0017a57943';
+
+const api = new Api(token, userId);
+const user = { email: 'vasia2@mail.ru', password: 'Puzzle123!' };
+const word = { difficulty: 'weak', optional: { testFieldString: 'test', testFieldBoolean: true } };
+
+// api.createUserWord('5e9f5ee35eb9e72bc21af716', word).then((d) => console.log(d));
+api.getWords().then((d) => console.log(d));
+
+// api.createUser(user).then((d) => console.log(d));
+// api.loginUser(user).then((d) => console.log(d, api.token, api.userId));
