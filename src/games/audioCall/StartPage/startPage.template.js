29cd41da012b4b22f7615629dfad053e4b0b3009
julia-dream <55105383+julia-dream@users.noreply.github.com>
2020-07-06 00:40:28 +0300

    audiocall/feat: att start page responsive

--- src/games/audioCall/StartPage/startPage.template.js
+++ src/games/audioCall/StartPage/startPage.template.js
@@ -2,17 +2,17 @@ export default function createStartPage() {
   return `
   <div class="container d-flex flex-column">
     <div class="row" style="padding-top: 5%;">
-        <div class="col-11"></div>
+        <div class="col-10 col-sm-11"></div>
         <div class="col">
             <button class="btn btn-close" type="button" data-event="close">
             <i class="fas fa-times" data-event="close"></i>
             </button>
         </div>
     </div>
-    <div class="row" style ="margin-top: 10%;">
+    <div class="row d-flex flex-wrap" style ="margin-top: 10%;">
         <div class="col"></div>
-        <div class="col">
-            <p class="my-5 text-uppercase text-center text-white display-4 game-title">
+        <div class="col-12">
+            <p class="my-5 text-uppercase text-center text-white display-5 game-title">
             Аудиовызов
             </p>
         </div>
@@ -25,9 +25,9 @@ export default function createStartPage() {
             </p>
         </div>
     </div>
-    <div class="row my-5">
-        <div class="col"></div>
-        <div class="col">
+    <div class="row my-5 d-flex flex-wrap">
+        <div class="col-3"></div>
+        <div class="col-12">
             <button type="button" 
             class="d-block btn btn-outline-light ml-auto mr-auto text-uppercase btn-start-game"
             data-event="startGame"
