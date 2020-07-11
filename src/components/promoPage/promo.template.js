570d8b42ed3606bd21e5a6ca7a4b8a29b0b3fb0c
Alexandr Voytekhovich <avth.tec@gmail.com>
2020-07-11 22:09:05 +0300

    promo-page/feat: add information about all games

--- src/components/promoPage/promo.template.js
+++ src/components/promoPage/promo.template.js
@@ -1,3 +1,49 @@
+import promoGames from './games-info';
+
+function createMembersHTML(data) {
+  const html = data.map((info) => {
+    const {
+      id, name, img, preview, description,
+    } = info;
+    return `
+    <div class="promo__game-description promo__developer-${id}">
+    <h3>${name}</h3>
+    <span class="promo__preview">${preview}</span>
+      <div class="promo__slider">
+        <div id="carouselExampleControls_${id}" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100 h-800px" src="${img[0]}" alt="First slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100 h-800px" src="${img[1]}" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100 h-800px" src="${img[2]}" alt="Third slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100 h-800px" src="${img[3]}" alt="Fourth slide">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls_${id}" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls_${id}" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+
+      <span class="promo__description">${description}</span>
+
+    </div>
+    `;
+  });
+  return html.join('');
+}
+
 export default function createPromoPageLayout() {
   return `
     <div class="promo__main-container">
@@ -14,39 +60,7 @@ export default function createPromoPageLayout() {
         </span>
 
       </div>
-
-      <div class="promo__game-description">
-      <h3>"Sprint" mini-game</h3>
-
-        <div class="col-sm-4 mx-auto">
-          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img class="d-block w-100" src="https://image.freepik.com/free-vector/glitch-error-404-page-background_23-2148085612.jpg" alt="First slide">
-              </div>
-              <div class="carousel-item">
-                <img class="d-block w-100" src="https://image.freepik.com/free-vector/glitch-error-404-page-background_23-2148085613.jpg" alt="Second slide">
-              </div>
-              <div class="carousel-item">
-                <img class="d-block w-100" src="https://image.freepik.com/free-vector/glitch-error-404-page_23-2148105403.jpg" alt="Third slide">
-              </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
-        </div>
-
-        <span>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, aliquam, sapiente cumque architecto excepturi quae cum officiis illo similique maxime quis aspernatur molestiae non sint ipsa libero cupiditate necessitatibus quod sequi! Rem reprehenderit nisi obcaecati ex eum possimus inventore quibusdam at, magnam recusandae provident, eaque similique, omnis iure accusantium aliquid deserunt vitae? Reprehenderit, aliquid? Ducimus culpa animi explicabo atque harum amet, perferendis laudantium mollitia odit rerum quasi, sint quo aspernatur ab placeat vitae itaque consequuntur. A dolorum, neque incidunt id omnis pariatur obcaecati sapiente aperiam maxime quas aspernatur quo laudantium, perspiciatis porro facere voluptatum tenetur at debitis nam perferendis? Perferendis illum quos est. Repudiandae beatae modi atque corporis asperiores repellendus sunt voluptas delectus dolore. Laudantium earum ipsum rem perferendis quae placeat quisquam, ipsa eos libero, cumque reprehenderit? Pariatur ea molestias quod, odit in voluptatibus, neque, numquam nam dicta voluptatum eligendi voluptatem et eum accusamus minima consequatur. Neque tenetur veritatis illo temporibus et debitis nemo eius accusamus assumenda aliquam fugit, animi nesciunt beatae enim maxime ipsum cum impedit corrupti? Impedit cumque eos deserunt molestias? Cum eaque maxime, deleniti harum culpa quam. Unde ex, fuga, fugiat facilis assumenda rem nisi doloribus hic similique ratione excepturi amet dolore exercitationem eius, in odit totam.
-        </span>
-
-      </div>
+      ${createMembersHTML(promoGames)}
 
     </div>
   `;
