ff16d1660d9c8cf6ed83dfe9881262a2c52eb6bd
Alexandr Voytekhovich <avth.tec@gmail.com>
2020-07-12 22:22:33 +0300

    sprint-mini-game/fix: fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
