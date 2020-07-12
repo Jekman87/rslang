461c156fc4c36f9cf1e379d14899aac1609f21d3
Slava Torchik <torhik_slava@mail.ru>
2020-07-12 16:13:35 +0300

    statistics/feat: add jumbotron container

--- src/components/statisticsPage/statisticsPage.template.js
+++ src/components/statisticsPage/statisticsPage.template.js
@@ -1,15 +1,17 @@
 export default function createStatisticsHTML() {
   return `
   <div class="container mt-3">
-    <h3 class="stat-header">Статистика главного приложения:</h3>
-    <p>Пока непонятно, что тут будет</p>
+    <div class="jumbotron mb-0">
+      <h3 class="stat-header">Статистика главного приложения:</h3>
+      <p>Пока непонятно, что тут будет</p>
 
-    <h3 class="stat-header">Статистика мини-игр:</h3>
-    <div class="tables-tab-wrapper">
-    </div>
+      <h3 class="stat-header">Статистика мини-игр:</h3>
+      <div class="tables-tab-wrapper">
+      </div>
 
-    <h3 class="stat-header">Визуальная статистика:</h3>
-    <div class="charts-tab-wrapper">
+      <h3 class="stat-header">Визуальная статистика:</h3>
+      <div class="charts-tab-wrapper">
+      </div>
     </div>
   </div>
   `;
