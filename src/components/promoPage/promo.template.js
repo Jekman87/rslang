a00fc5fe19238cda7f516f4dbe09f061a6d96e5c
Alexandr Voytekhovich <avth.tec@gmail.com>
2020-07-14 02:39:15 +0300

    promo-page/style: change responsive styles

--- src/components/promoPage/promo.template.js
+++ src/components/promoPage/promo.template.js
@@ -26,11 +26,11 @@ function createMembersHTML(data) {
               </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleControls_${id}" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="carousel-control-prev-icon control-color-left-${id}" aria-hidden="true"></span>
               <span class="sr-only">Previous</span>
             </a>
             <a class="carousel-control-next" href="#carouselExampleControls_${id}" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="carousel-control-next-icon control-color-right-${id}" aria-hidden="true"></span>
               <span class="sr-only">Next</span>
             </a>
           </div>
@@ -51,15 +51,24 @@ export default function createPromoPageLayout() {
 
       <div class="promo__training-description">
 
-        <h2>RSLang</h2>
-
-        <div class="my_video">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/l0dWFPbf28A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <h2> <i class="fas fa-chart-pie"></i> RSLang</h2>
+        <div class="row justify-content-center no-gutters mt-3">
+          <div class="col-10 col-md-8">
+            <div class="my_video">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/l0dWFPbf28A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          </div>
         </div>
 
         <span>
           Рады приветствовать всех, кто заглянул в RSLang - добро пожаловать на маленький островок во вселенной лингвистики. Наше приложение направлено на изучение английского языка, улучшения навыков произношения и восприятия на слух, обновления существующих знаний и развития кругозора. В базовой основе нашего приложения лежит методика интервального повторения.<br>
-          Не смотря на всю серьёзность научного подхода изучения языка по этому принципу, в арсенале нашего приложения имеются мини-игры, которые дадут возможность провести время с пользой, закрепить и обновить ваши лингвистические навыки, а также не дадут заскучать и сохранить мотивацию к изучению языка. Ниже вы можете ознакомиться со всеми дополнительными составляющими нашего приложения.
+          Не смотря на всю серьёзность научного подхода изучения языка по этому принципу, в арсенале нашего приложения имеются мини-игры, которые дадут возможность провести время с пользой, закрепить и обновить ваши лингвистические навыки, а также не дадут заскучать и сохранить мотивацию к изучению языка.<br>
+          Далее по тексту вы можете ознакомиться со всеми дополнительными составляющими нашего приложения, также как и с исходным кодом приложения, который вы сможете найти в нашем репозитории по ссылке ниже.
+        </span>
+
+        <span>
+          <i class="fab fa-github"></i>
+          <a href="https://github.com/Jekman87/rslang">RSLang repository</a>
         </span>
 
       </div>
