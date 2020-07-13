ae666fc3d0b9ae3356019e3c78c2fc3deecaac13
ayaki-coder <sj.a.yakimovich@gmail.com>
2020-07-14 00:08:18 +0300

    settings/refactor: add spaces

--- src/components/settingsPage/settingsPage.template.js
+++ src/components/settingsPage/settingsPage.template.js
@@ -14,13 +14,13 @@ export default function createSettingsHTML() {
               <div class="d-flex flex-column-reverse flex-lg-row align-items-start  align-items-xl-baseline mb-2 mb-lg-1">
                 <p class="d-flex justify-content-start align-items-baseline mb-lg-1">
               <input type="text" class="form-control mr-1 text-primary" id="settingsPageWordsPerDay" title="Карточек должно быть минимум в 2,5 раза больше чем новых слов" style="width: 4rem" value="" autocomplete="off" maxlength="3">шт</p>
-                <label class="col-form-label col-12 col-lg-9" for="settingsPageWordsPerDay">Количество новых слов в день<span class="text-muted">(от 1 до 200)</span></label>
+                <label class="col-form-label col-12 col-lg-9" for="settingsPageWordsPerDay">Количество новых слов в день<span class="text-muted"> (от 1 до 200)</span></label>
 
               </div>
               <div class="d-flex flex-column-reverse flex-lg-row align-items-start align-items-lg-end  align-items-xl-baseline mb-2">
               <p class="d-flex justify-content-start align-items-baseline">
                 <input type="text" class="form-control mr-1 text-primary" id="settingsPageCardsPerDay" title="Карточек должно быть минимум в 2,5 раза больше чем новых слов" style="width: 4rem" value="" autocomplete="off" maxlength="3">шт</p>
-                <label class="col-form-label col-12 col-lg-9" for="settingsPageCardsPerDay">Максимальное количество карточек в день<span class="text-muted">(от 3 до 500)</span></label>
+                <label class="col-form-label col-12 col-lg-9" for="settingsPageCardsPerDay">Максимальное количество карточек в день<span class="text-muted"> (от 3 до 500)</span></label>
 
               </div>
               <div class="custom-control custom-radio mb-2 mb-md-3 mb-lg-2">
