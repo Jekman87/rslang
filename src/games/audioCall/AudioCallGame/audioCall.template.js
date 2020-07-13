ecdcb28346c83c0bf7113799c68d04cd330ac9b1
julia-dream <55105383+julia-dream@users.noreply.github.com>
2020-07-13 14:51:45 +0300

    audiocall/feat: fix some styles

--- src/games/audioCall/AudioCallGame/audioCall.template.js
+++ src/games/audioCall/AudioCallGame/audioCall.template.js
@@ -25,7 +25,7 @@ export default function createAudioCall() {
         <i class="fas fa-music" data-event="soundOff"></i>
         <i class="fas fa-slash d-none" data-event="soundOff"></i>
       </button>
-      
+
     </div>
     <div class="col-6 col-sm-9"></div>
     <div class="col">
@@ -41,7 +41,7 @@ export default function createAudioCall() {
           <i class="fas fa-volume-up" data-event="repeat"></i>
         </button>
     </div>
-    <div class="col d-none answer-col"> 
+    <div class="col d-none answer-col">
       <button class="btn btn-repeat d-block ml-auto mr-auto on-answer" type="button" data-event="repeat">
         <i class="fas fa-volume-up" data-event="repeat"></i>
       </button>
@@ -50,12 +50,12 @@ export default function createAudioCall() {
         <img class= "answer-pic" src="">
         <p class="text-white answer-word"></p>
       </div>
-      
+
     </div>
     <div class="col"></div>
   </div>
   <div class="row group-list-row">
-    <div class="col">
+    <div class="col buttons-col">
       <div class="btn-group w-100 d-flex flex-wrap flex-lg-nowrap" role="group" aria-label="">
         <button type="button" class="btn btn-outline-light mr-lg-5 ml-lg-5 btn-word">
           <span class="mr-2 round-word-number"></span> <span class="round-word"></span>
@@ -78,13 +78,13 @@ export default function createAudioCall() {
   <div class="row my-5">
         <div class="col-4 col-sm-4 col-lg-4"></div>
         <div class="col-sm-4 col-md">
-            <button type="button" 
+            <button type="button"
             class="btn btn-outline-light ml-auto mr-auto text-uppercase btn-dont-know"
             data-event="dontKnow"
             >
             <span class="px-2" data-event="dontKnow">не знаю</span>
             </button>
-            <button type="button" 
+            <button type="button"
             class="d-none btn btn-outline-light ml-auto mr-auto text-uppercase btn-next"
             data-event="next"
             >
